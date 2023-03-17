8c503d1ede9807791b0aea786d4423e609458819
Jubril225 <Ayomidejubril225@gmail.com>
2023-03-16 19:21:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+ /**
+  * main - Entry point
+  * puts - Output stream
+  * Return; Always 0
+  */
+
+  int main(void) 
+  {
+   char var_1 = "\"Programming is like building a multilingual puzzle";
+
+   puts(var_1);
+   return (0);
+   }
