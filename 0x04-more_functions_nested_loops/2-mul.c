18a15d184f023e05e123d721360a716cbe73c2a0
Jubril225 <Ayomidejubril225@gmail.com>
2023-03-23 16:12:04 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * nul: Multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Addition of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
