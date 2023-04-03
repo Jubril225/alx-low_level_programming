b33f8dcb2d63b0df2daa81d72da4cc62182509ca
Jubril225 <Ayomidejubril225@gmail.com>
2023-04-03 14:57:38 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strstr -  function finds the first occurrence of the substring needle in the string haystack
+ * @haystack: input
+ * @needle: input
+ *
+ * Return: Always 0 (success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
