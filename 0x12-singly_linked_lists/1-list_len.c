c8751b2a9bfe8e18597f6be92c61bfe33721ce8a
jubril225 <ayomidejubril225@gmail.com>
2023-04-27 07:55:47 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len -  a function that returns the number of elements
+ * @h: pointer to the node
+ *
+ * Return: number of element in h
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
