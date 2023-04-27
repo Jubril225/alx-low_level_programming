629c2254cde3b3ad1db6ddeb8ff5c5fd3df802ca
jubril225 <ayomidejubril225@gmail.com>
2023-04-27 14:04:42 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,12 +2,12 @@
 #include "lists.h"
 
 /**
- * list_len -  a function that returns the number of elements
- * @h: pointer to the node
- *
- * Return: number of element in h
- *
- */
+* list_len -  a function that returns the number of elements hyth hyyhgff h for yhr account be the
+* @h: pointer to the node  
+*
+* Return: number of element in h
+*
+*/
 
 size_t list_len(const list_t *h)
 {
