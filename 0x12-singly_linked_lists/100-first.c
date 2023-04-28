636242c2d6392e27d9b1587dbfb665089201d527
jubril225 <ayomidejubril225@gmail.com>
2023-04-28 04:15:43 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * first -  a function that prints You're beat!
+ * function is executed
+ *
+ * Return: Always 0
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
